4702dda946b6f623684588761e2bb88f9ab1783d
elazsyahranie <elazaribrahims@gmail.com>
2021-10-13 19:19:28 +0700

    Fixed token expire again

--- src/modules/auth/auth_controller.js
+++ src/modules/auth/auth_controller.js
@@ -106,7 +106,7 @@ module.exports = {
           delete payload.user_password
           delete payload.user_pin
           const token = jwt.sign({ ...payload }, process.env.PRIVATE_KEY, {
-            expiresIn: '25s'
+            expiresIn: '24h'
           })
 
           const result = { ...payload, token }

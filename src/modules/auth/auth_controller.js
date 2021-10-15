9f1c75a21f12032a0a0ee25e3f115f7a8b90c603
elazsyahranie <elazaribrahims@gmail.com>
2021-10-15 10:49:28 +0700

    Refresh token

--- src/modules/auth/auth_controller.js
+++ src/modules/auth/auth_controller.js
@@ -120,6 +120,7 @@ module.exports = {
           // Memasukkan data checkUserEmail ke dalam refreshToken
           dataRefreshToken[checkUserEmail[0].user_id] = refreshToken
           console.log('Refresh the Token!')
+          console.log(checkUserEmail)
           console.log(dataRefreshToken)
           const result = { ...payload, token, refreshToken }
           return helper.response(res, 200, 'Succes Login !', result)

eac6a417a66992ba4da4c80c1fc6ba04297a48ef
elazsyahranie <elazaribrahims@gmail.com>
2021-10-17 14:19:00 +0700

    Fixed redis deploy

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -1,5 +1,14 @@
 const redis = require('redis')
-const client = redis.createClient()
+const client = redis.createClient({
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD
+})
+
+client.on('connect', () => {
+  console.log('Connected to our redis instance!')
+  client.set('Greatest Basketball Player', 'Lebron James')
+})
 const helper = require('../helpers/wrapper')
 // const helperUser = require('../helpers/wrapperUser')
 
